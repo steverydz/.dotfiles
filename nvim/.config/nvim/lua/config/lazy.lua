186427451b37cbed589b421f5894bd04e73cdbf6
Steve Rydz <steverydz@gmail.com>
2024-10-13 23:22:19 +0100

    Cleanup

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -50,18 +50,6 @@ require('lazy').setup({
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
   -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
   { 'Bilal2453/luvit-meta', lazy = true },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
