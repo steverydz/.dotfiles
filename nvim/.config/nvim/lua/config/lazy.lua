69a4643fb500d8eb4767a6bd57e96a0e5aaa4046
Steve Rydz <steverydz@gmail.com>
2024-10-13 23:23:25 +0100

    Remove plugin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -49,9 +49,6 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- LSP Plugins
-  { 'Bilal2453/luvit-meta', lazy = true },
-
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
