021b4cd997f1921098d180c36d3d4db3aee0b79f
Steve Rydz <steverydz@gmail.com>
2024-10-14 12:34:57 +0100

    Prevent notification on lazy changes

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -9,5 +9,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup {
-  { import = 'plugins' },
+  spec = 'plugins',
+  change_detection = { notify = false },
 }
