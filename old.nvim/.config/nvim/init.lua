a32ee4fc673f74c0f3ff5b741439869b9956a3de
Steve Rydz <steverydz@gmail.com>
2024-11-22 19:01:36 +0000

    Replace Neovim config with Kickstart

---
+++ old.nvim/.config/nvim/init.lua
@@ -0,0 +1,11 @@
+require 'config.remap'
+require 'config.set'
+require 'config.lazy'
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
