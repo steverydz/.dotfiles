6220e95ac185d6a56f299ed00569b7250a28585e
Steve Rydz <steverydz@gmail.com>
2024-11-24 19:08:06 +0000

    Remove old Neovim config

--- old.nvim/.config/nvim/lua/plugins/color.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'shaunsingh/nord.nvim',
-  priority = 1000,
-  init = function()
-    vim.g.nord_italic = false
-    vim.cmd.colorscheme 'nord'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
