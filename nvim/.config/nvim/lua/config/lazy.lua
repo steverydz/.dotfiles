ce11df88bb25c58725d2d151a0dede8290dd17f6
Steve Rydz <steverydz@gmail.com>
2024-12-15 19:23:21 +0000

    Add some new remaps and trouble to nvim config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -13,4 +13,5 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	import = "plugins",
+	change_detection = { notify = false },
 })
--- nvim/.config/nvim/lua/config/remaps.lua
+++ nvim/.config/nvim/lua/config/remaps.lua
@@ -5,3 +5,10 @@ vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
+
+vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>")
