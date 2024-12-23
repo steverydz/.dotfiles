bd593113975241bc863337fe1bd0d5a1a4bc5d2b
Steve Rydz <steverydz@gmail.com>
2024-12-23 11:29:59 +0000

    Clean up Neovim autoformat config

--- nvim/.config/nvim/lua/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -14,17 +14,10 @@ return {
 	},
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
-			local lsp_format_opt
-			if disable_filetypes[vim.bo[bufnr].filetype] then
-				lsp_format_opt = "never"
-			else
-				lsp_format_opt = "fallback"
-			end
+		format_on_save = function()
 			return {
 				timeout_ms = 500,
-				lsp_format = lsp_format_opt,
+				lsp_format = "fallback",
 			}
 		end,
 		formatters_by_ft = {
