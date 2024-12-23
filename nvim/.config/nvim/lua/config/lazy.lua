e3f753a1538a59fbe8220a148e2d59e5e170ca4b
Steve Rydz <steverydz@gmail.com>
2024-12-23 15:21:10 +0000

    Fix change detection

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,8 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 
 	if vim.v.shell_error ~= 0 then
 		error("Error cloning lazy.nvim:\n" .. out)
@@ -12,6 +18,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	import = "plugins",
-	change_detection = { notify = false },
+	spec = "plugins",
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
 })
