8c1b4a1b2a791701936599ab7cd8a54e1bd48201
Steve Rydz <steverydz@gmail.com>
2024-12-23 19:31:03 +0000

    Add Neovim keymaps

--- nvim/.config/nvim/lua/config/remaps.lua
+++ nvim/.config/nvim/lua/config/remaps.lua
@@ -1,8 +1,43 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>")
+
+-- Move highlighted line up/down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
+-- Keep cursor in same position when appending next line
+vim.keymap.set("n", "J", "mzJ`z")
 
-vim.keymap.set("n", "<leader><leader>", "<cmd>so<CR>")
+-- Keep cursor in center position when scrolling up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor in center position when incrementing/decrementing search
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Keep previous yank register
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Yank to system copy register
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+
+-- Never press Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Format file
+vim.keymap.set("n", "<leader>f", function()
+	vim.lsp.buf.format()
+end)
+
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
