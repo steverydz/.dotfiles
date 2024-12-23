82dae9ba86f7201d1c0372a6241f79787e857fa2
Steve Rydz <steverydz@gmail.com>
2024-12-23 21:28:47 +0000

    Add more fugitive keybindings

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return {
 		vim.keymap.set("n", "gw", "<cmd>Gwrite<CR>")
 		vim.keymap.set("n", "gr", "<cmd>Gread<CR>")
 		vim.keymap.set("n", "gc", "<cmd>Git commit<CR>")
+		vim.keymap.set("n", "gp", "<cmd>Git push<CR>")
 	end,
 }
