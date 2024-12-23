2d42ed191768151bea50db07946cb66430ba6340
Steve Rydz <steverydz@gmail.com>
2024-12-23 21:34:32 +0000

    Add fugitive keymaps for fetching

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -5,5 +5,7 @@ return {
 		vim.keymap.set("n", "gr", "<cmd>Gread<CR>")
 		vim.keymap.set("n", "gc", "<cmd>Git commit<CR>")
 		vim.keymap.set("n", "gp", "<cmd>Git push<CR>")
+		vim.keymap.set("n", "gfo", "<cmd>Git fetch origin<CR>")
+		vim.keymap.set("n", "gfu", "<cmd>Git fetch upstream<CR>")
 	end,
 }
