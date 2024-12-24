7a09a56622004eeef3cf32a55a083d437b0845b2
Steve Rydz <steverydz@gmail.com>
2024-12-24 12:01:43 +0000

    Added comments to fugitive keymaps

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,8 +1,12 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
+		-- :Git add %
 		vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<CR>")
+
+		-- :Git checkout %
 		vim.keymap.set("n", "<leader>gr", "<cmd>Gread<CR>")
+
 		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
 		vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
 		vim.keymap.set("n", "<leader>gfo", "<cmd>Git fetch origin<CR>")
