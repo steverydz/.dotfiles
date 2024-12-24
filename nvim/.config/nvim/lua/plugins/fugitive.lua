70f63ff30ac093f9601e032f96060a4bec95cf7a
Steve Rydz <steverydz@gmail.com>
2024-12-24 09:42:27 +0000

    Edit keymaps for vim fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,11 +1,11 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "gw", "<cmd>Gwrite<CR>")
-		vim.keymap.set("n", "gr", "<cmd>Gread<CR>")
-		vim.keymap.set("n", "gc", "<cmd>Git commit<CR>")
-		vim.keymap.set("n", "gp", "<cmd>Git push<CR>")
-		vim.keymap.set("n", "gfo", "<cmd>Git fetch origin<CR>")
-		vim.keymap.set("n", "gfu", "<cmd>Git fetch upstream<CR>")
+		vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<CR>")
+		vim.keymap.set("n", "<leader>gr", "<cmd>Gread<CR>")
+		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
+		vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
+		vim.keymap.set("n", "<leader>gfo", "<cmd>Git fetch origin<CR>")
+		vim.keymap.set("n", "<leader>gfu", "<cmd>Git fetch upstream<CR>")
 	end,
 }
