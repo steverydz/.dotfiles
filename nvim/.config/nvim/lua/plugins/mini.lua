7d76b3cd14380471758da235ca8f6f681d0c426e
Steve Rydz <steverydz@gmail.com>
2025-01-13 21:48:07 +0000

    Add mini surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,20 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		require("mini.statusline").setup()
-		require("mini.comment").setup()
 		require("mini.pairs").setup()
+
+		-- mini.comment keybindings
+		-- gcc    - toggle comment
+		-- gc     - comment (Visual mode)
+		require("mini.comment").setup()
+
+		-- mini.surround keybindings
+		-- va'    - Visually select around '
+		-- ci'    - Change inside '
+		-- sa'    - Surround '
+		-- saiw'  - Surround add inner word '
+		-- sd'    - Surround delete '
+		-- sr'"   - Surround replace ' with "
+		require("mini.surround").setup()
 	end,
 }
