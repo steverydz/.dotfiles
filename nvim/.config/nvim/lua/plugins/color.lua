7a82dc9b950ccb06611b91077d609d269893075d
Steve Rydz <steverydz@gmail.com>
2025-01-24 09:55:24 +0000

    Revert Neovim colorscheme to Nord

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,14 +1,8 @@
 return {
-	"rose-pine/neovim",
-	lazy = false,
+	"shaunsingh/nord.nvim",
 	priority = 1000,
 	config = function()
-		--- @diagnostic disable-next-line missing-fields
-		require("rose-pine").setup({
-			styles = {
-				italic = false,
-			},
-		})
-		vim.cmd.colorscheme("rose-pine-moon")
+		vim.g.nord_italic = false
+		vim.cmd.colorscheme("nord")
 	end,
 }
