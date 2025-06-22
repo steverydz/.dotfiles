91045d7b9be32ace22478cff316dfc36b04c5ccf
Steve Rydz <steverydz@gmail.com>
2025-06-22 13:11:46 +0100

    Change colour scheme to rose-pine

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,10 +1,25 @@
 return {
-  "olimorris/onedarkpro.nvim",
+  "rose-pine/neovim",
   lazy = false,
   priority = 1000,
   opts = {
   },
   config = function()
-    vim.cmd.colorscheme("onedark")
+    require("rose-pine").setup({
+      variant = "auto",      -- auto, main, moon, or dawn
+      dark_variant = "main", -- main, moon, or dawn
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+      enable = {},
+      styles = {
+        italic = false,
+      },
+      groups = {},
+      palette = {},
+      highlight_groups = {},
+      before_highlight = function()
+      end,
+    })
+    vim.cmd.colorscheme("rose-pine")
   end,
 }
